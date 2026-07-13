24d48cffe7f9f230a1f87dd662d29ae2232ac5b3
Jonathan Juayong <jonathan.juayong@gmail.com>
2026-07-13 12:09:54 +1000

    implement item highlighting

--- src/store.ts
+++ src/store.ts
@@ -14,3 +14,21 @@ export function getSong() {
 export function subscribeToSong(callback: (newValue: Readonly<Song> | null, oldValue?: Readonly<Song> | null | undefined) => void) {
     $chordText.subscribe(callback)
 }
+
+const $highlightedItem = atom<{line: number | null, item: number | null}>({
+    line: null,
+    item: null,
+})
+
+export function setHighlightedItem(line: number, item: number) {
+    const newItem = {line, item}
+    $highlightedItem.set(newItem)
+}
+
+export function getCurrentHighlightedItem() {
+    return $highlightedItem.get()
+}
+
+export function subscribeToHighlightedItem(callback: (newValue: Readonly<{line: number | null, item: number | null}>, oldValue?: Readonly<{line: number | null, item: number | null}>) => void) {
+    return $highlightedItem.subscribe(callback)
+}
