6386f84d133b033e647f7dd988b4b7e76b50fbee
Jonathan Juayong <jonathan.juayong@gmail.com>
2026-07-25 20:28:55 +1000

    include artist in generated id to avoid conflicts with songs that have the same title

--- src/content.config.ts
+++ src/content.config.ts
@@ -5,7 +5,18 @@ import {z} from "astro/zod";
 const songs = defineCollection({
     loader: glob({
         pattern: "**/*.md",
-        base: "./src/songs"
+        base: "./src/songs",
+        generateId: ({data}) => {
+            const artist = data?.artist as string | undefined
+            const title = data?.title as string | undefined
+
+            if (!artist || !title) return ""
+
+            const artistSlug = artist.replaceAll(" ", "-").toLowerCase().trim()
+            const titleSlug = title.replaceAll(" ", "-").toLowerCase().trim()
+
+            return `${titleSlug}-${artistSlug}`
+        }
     }),
     schema: z.object({
        title: z.string(),
