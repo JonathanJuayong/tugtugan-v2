69eb6aafb3284cb9cb46c2142a584e94d74a843f
Jonathan Juayong <jonathan.juayong@gmail.com>
2026-07-26 11:49:37 +1000

    remove line wrapping

--- src/components/svelte/ChordSheetLineItem.svelte
+++ src/components/svelte/ChordSheetLineItem.svelte
@@ -46,9 +46,9 @@
 
     </pre>
 {:else if item.chords === '' && item.lyrics.length > 0}
-    <pre class={["grid content-end transition-all", item.isHighlighted ? highlight : '']} {id}><button {onclick} class={"cursor-pointer whitespace-pre-wrap"}>{item.lyrics}</button></pre>
+    <pre class={["grid content-end transition-all", item.isHighlighted ? highlight : '']} {id}><button {onclick} class={"cursor-pointer"}>{item.lyrics}</button></pre>
 {:else if item.lyrics === '' && item.chords.length > 0}
-    <pre data-chord={item.chords} class={["grid content-start transition-all", item.isHighlighted ? highlight : '']} {id}><button {onclick} class="cursor-pointer whitespace-pre-wrap">{item.chords}</button></pre>
+    <pre data-chord={item.chords} class={["grid content-start transition-all", item.isHighlighted ? highlight : '']} {id}><button {onclick} class="cursor-pointer">{item.chords}</button></pre>
 {:else}
     <pre class={["transition-all", item.isHighlighted ? highlight : '']} data-chord={item.chords} {id}><button {onclick} class="grid whitespace-nowrap cursor-pointer">
         <span>{item.chords}</span>
